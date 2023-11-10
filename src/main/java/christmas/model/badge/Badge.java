9b11e822ddb39f31c54dcb0d1ad69cec3b1086db
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-11-11 02:56:44 +0900

    feat(Badge): 배지 데이터 세팅 및 총혜택 금액에 따른 배지 반환 기능 구현

---
+++ src/main/java/christmas/model/badge/Badge.java
@@ -0,0 +1,25 @@
+package christmas.model.badge;
+
+import java.util.Arrays;
+
+public enum Badge {
+    SANTA("산타", 20_000),
+    TREE("트리", 10_000),
+    STAR("별", 5_000),
+    NONE("없음", 0);
+
+    private final String name;
+    private final int standardAmount;
+
+    Badge(String name, int standardAmount) {
+        this.name = name;
+        this.standardAmount = standardAmount;
+    }
+
+    public static Badge getBadgeByTotalBenefit(final int amount) {
+        return Arrays.stream(values())
+                .filter(badge -> badge.standardAmount <= amount)
+                .findFirst()
+                .orElse(NONE);
+    }
+}
