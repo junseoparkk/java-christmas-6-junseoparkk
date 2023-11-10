dd51bbf407446abd3ee0ebb49f1a7fc20de26132
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-11-11 04:13:57 +0900

    feat(InputValidator): 주문 메뉴 형식 검증 기능 구현

---
+++ src/main/java/christmas/exception/InvalidOrderException.java
@@ -0,0 +1,9 @@
+package christmas.exception;
+
+public class InvalidOrderException extends IllegalArgumentException {
+    private static final String MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해주세요.";
+
+    public InvalidOrderException() {
+        super(MESSAGE);
+    }
+}
