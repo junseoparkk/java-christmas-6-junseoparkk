0f2fef15c5868980b6f4a84df76c085e74b9e7d2
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-11-12 07:35:31 +0900

    feat(DiscountPolicy): 주말 할인 정책 기능 구현

---
+++ src/main/java/christmas/model/event/WeekendDiscountPolicy.java
@@ -0,0 +1,18 @@
+package christmas.model.event;
+
+import christmas.model.menu.MenuCategory;
+import christmas.model.order.Order;
+
+public class WeekendDiscountPolicy implements DiscountPolicy {
+    private static final int BASIC_DISCOUNT_AMOUNT = 2023;
+
+    @Override
+    public EventCategory getEventCategory() {
+        return EventCategory.WEEK_END_DISCOUNT;
+    }
+
+    @Override
+    public int calculateDiscountAmount(Order order) {
+        return BASIC_DISCOUNT_AMOUNT * order.countMenuByCategory(MenuCategory.MAIN);
+    }
+}
