3a5cc46bfd9c6da282e6b9a7e5c8a9014012ac07
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-11-13 23:36:57 +0900

    style(Exception): 에러 메시지 형식 띄어쓰기 추가

--- src/main/java/christmas/exception/InvalidOrderException.java
+++ src/main/java/christmas/exception/InvalidOrderException.java
@@ -1,7 +1,7 @@
 package christmas.exception;
 
 public class InvalidOrderException extends IllegalArgumentException {
-    private static final String MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해주세요.";
+    private static final String MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
     public InvalidOrderException() {
         super(MESSAGE);
