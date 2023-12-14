d00bcf894cda7997f4ac817c001a3eaa02b05651
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-12-15 02:30:19 +0900

    feat(InputValidator): 주문 메뉴 및 개수 입력 형식 검증 기능 구현

--- src/main/java/christmas/utils/validator/InputValidator.java
+++ src/main/java/christmas/utils/validator/InputValidator.java
@@ -3,6 +3,7 @@ package christmas.utils.validator;
 public class InputValidator {
     private static final int MINIMUM_DAY = 1;
     private static final int MAXIMUM_DAY = 31;
+    private static final String MENUS_INPUT_REGEX = "^([가-힣]+-\\d)+(?:,([가-힣]+-\\d)+)*$";
 
     private InputValidator() {
     }
@@ -10,6 +11,10 @@ public class InputValidator {
     public static void validateVisitDay(final String day) {
         int date = validateIsDigit(day);
         validateDayRange(date);
+    }
+
+    public static void validateMenus(final String menus) {
+        validateMenusForm(menus);
     }
 
     private static int validateIsDigit(final String day) {
@@ -26,6 +31,12 @@ public class InputValidator {
         }
         if (day > MAXIMUM_DAY) {
             throw new IllegalArgumentException("[ERROR] 31 이하의 숫자만 입력해 주세요.");
+        }
+    }
+
+    private static void validateMenusForm(final String menus) {
+        if (!menus.matches(MENUS_INPUT_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 올바른 입력 형식이 아닙니다.");
         }
     }
 }
