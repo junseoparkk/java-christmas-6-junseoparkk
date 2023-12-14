efddb15b1488df6ee82e2bcd0a06480e873e948a
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-12-15 02:25:55 +0900

    feat(Application): 프로그램 실행 기능 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,11 +1,10 @@
 package christmas;
 
-import christmas.controller.EventPlannerController;
+import christmas.controller.MainController;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationConfig applicationConfig = new ApplicationConfig();
-        EventPlannerController eventPlannerController = new EventPlannerController(applicationConfig);
-        eventPlannerController.run();
+        MainController mainController = new MainController();
+        mainController.process();
     }
 }
---
+++ src/main/java/christmas/controller/MainController.java
@@ -0,0 +1,9 @@
+package christmas.controller;
+
+import christmas.utils.InputHandler;
+
+public class MainController {
+    public void process() {
+        final int visitDay = InputHandler.receiveVisitDay();
+    }
+}
